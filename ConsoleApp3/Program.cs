aae14f5745284b65fecaef80e0ac58daac89cdc3
Kumic <Kumic@MAXDUST_PC>
2024-05-29 22:31:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,71 @@
+﻿public interface IMyInterfase<T>
+{
+    void Add(int index, T element);
+    T Get(int index);
+    void Remove(int index);
+}
+
+public class ListBimbimBamBam<T> : IMyInterfase<T>
+{
+    private T[] array;
+
+    public ListBimbimBamBam(int size)
+    {
+        array = new T[size];
+    }
+    public void Add(int index, T element)
+    {
+        if (index >= 0 && index < array.Length)
+        {
+            array[index] = element;
+        }
+        else
+        {
+            throw new IndexOutOfRangeException("Index is out of range");
+        }
+    }
+
+    public T Get(int index)
+    {
+        if (index >= 0 && index < array.Length)
+        {
+            return array[index];
+        }
+        else
+        {
+            throw new IndexOutOfRangeException("Index is out of range");
+        }
+    }
+
+    public void Remove(int index)
+    {
+        if (index >= 0 && index < array.Length)
+        {
+            array[index] = default(T);
+        }
+        else
+        {
+            throw new IndexOutOfRangeException("Index is out of range");
+        }
+    }
+
+    public void Sort()
+    {
+        Array.Sort(array);
+    }
+
+    public int Length
+    {
+        get { return array.Length; }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args) { 
+        ListBimbimBamBam<int> ints = new ListBimbimBamBam<int>(3);
+        ints.Sort();
+        ints.Add(2,6);
+        Console.WriteLine(  ints.Length);
+    }
+}
